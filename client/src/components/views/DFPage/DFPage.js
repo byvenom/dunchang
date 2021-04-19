5cbb4090b4e284002c8581d77a8009cc435484b5
song <zmdk5650@gmail.com>
2021-04-19 15:41:19 +0900

    0419_3

--- client/src/components/views/DFPage/DFPage.js
+++ client/src/components/views/DFPage/DFPage.js
@@ -74,7 +74,7 @@ function DFPage() {
     return (
         <div style={{ maxWidth:'1200px', margin:'2rem auto'}}>
             <div style={{ textAlign:'center', marginBottom:'2rem'}}>
-                <Title level={2}><a href="/dunfa"><img src={logo} className="App-logo" alt=""/>DUNCHANG<img src={logo} className="App-logo2" alt=""/></a></Title>
+                <Title level={2}><a href="/dunfa"><img src={logo} style={{width:'36px' ,height:'36px'}}className="App-logo" alt=""/>DUNCHANG<img style={{width:'36px' ,height:'36px'}} src={logo} className="App-logo2" alt=""/></a></Title>
             </div> 
                 <Form>
 

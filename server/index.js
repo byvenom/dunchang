9910aa57305a03ebf3b9f13447802893d52e4f5d
song <zmdk5650@gmail.com>
2021-04-27 17:38:45 +0900

    rr

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use('/api/df', require('./routes/df'));
 //use this to show the image you have in node js server to client (react js)
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
-app.use("*", express.static(clientApp))
+
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
 
